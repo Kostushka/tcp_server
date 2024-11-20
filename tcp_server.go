9c73bca3765707ba2949921ac727c5637c89208c
kostushka <nastyatery47@yandex.ru>
2024-11-20 12:59:32 +0300

    update

--- tcp_server.go
+++ tcp_server.go
@@ -9,6 +9,7 @@ import (
 	"errors"
 	"os"
 	"io/fs"
+	"bufio"
 )
 
 type QueryString struct {
@@ -17,7 +18,9 @@ type QueryString struct {
 	protocol string
 }
 
-var path string = "/media/user/B4F1-F772"
+type RequestHeaders map[string]string
+
+var path string = "/home/kostushka"
 
 func main() {
 
@@ -40,53 +43,49 @@ func main() {
 		}
 		// обрабатываем каждое клиентское соединение в отдельной горутине
 		go func(conn *net.TCPConn) {
-			// буфер для чтения из клиентского сокета
-			buf := make([]byte, 4096)
 			
 			// структура с данными строки запроса HTTP-протокола
 			q := QueryString{}
+			// map с заголовками запроса
+			// reqhead := make(RequestHeaders)
+
+			// получить данные запроса
+			data := getRequestData(conn)
+				
+			fmt.Println("получили буфер", data)
+			// парсим строку запроса в структуру
+			parseQueryString(&q, data)
+		
+			// парсим заголовки запроса в map
+			// parseRequestHeaders(reqhead, string(buf))
 			
-			// пока клиентский сокет пишет, читаем в буфер
-			for  {
-				_, err := conn.Read(buf)
-				// по возвращении клиентским сокетом EOF, перестаем читать
-				if err == io.EOF {
-					break
-				}
-				if err != nil {
-					log.Fatal(err)
-				}
-				
-				// парсим строку запроса в структуру
-				parseQueryString(&q, string(buf))
-				
-				// открываем запрашиваемый файл
-				f, err := os.Open(path + q.path)
-				// файл должен быть
-				if errors.Is(err, fs.ErrNotExist) {
+			// открываем запрашиваемый файл
+			f, err := os.Open(path + q.path)
+			
+			// файл должен быть
+			switch {
+				case errors.Is(err, fs.ErrNotExist):
 					_, err := conn.Write([]byte(err.Error() + "\n"))
 					if err != nil {
 						log.Fatal(err)
 					}
-					continue
-				} else if err != nil {
+				case err != nil:
 					log.Fatal(err)
-				}
-
-				// читаем файл
-				fileBuf := make([]byte, 4096)
-				for {
-					_, err := f.Read(fileBuf)
-					if err == io.EOF {
-						break
-					}
-					if err != nil {
-						log.Fatal(err)
-					}
-					fmt.Println(string(fileBuf))
-				}
+				default:
+					// читаем файл
+					fileBuf := make([]byte, 4096)
+					for {
+						_, err := f.Read(fileBuf)
+						if err == io.EOF {
+							break
+						}
+						if err != nil {
+							log.Fatal(err)
+						}
+						conn.Write(fileBuf)
+					}		
 			}
-						
+			
 			// закрыть клиентское соединение
 			conn.Close()
 		}(conn)
@@ -94,11 +93,54 @@ func main() {
 }
 
 func parseQueryString(q *QueryString, str string) {
-	buf := strings.Split(str, " ")
+	// читаем строку из буфера
+	query, err := bufio.NewReader(strings.NewReader(str)).ReadString('\n')
+	if err != nil && err != io.EOF {
+		log.Fatal(err)
+	}
+	
+	buf := strings.Split(query, " ")
 	if len(buf) < 3 {
 		log.Fatal(errors.New("not HTTP"))
 	}
 	q.method = buf[0]
 	q.path = buf[1]
 	q.protocol = buf[2]
+
+	query, err = bufio.NewReader(strings.NewReader(str)).ReadString('\n')
+	fmt.Println(query)
+}
+
+// func parseRequestHeaders(reqhead map[string]string, str string) {
+	// buf := strings.Split(str, " ")
+	// fmt.Println(buf)
+// }
+
+func getRequestData(conn *net.TCPConn) string {
+	// буфер для чтения из клиентского сокета
+	buf := make([]byte, 4096)
+	
+	var data string
+	// пока клиентский сокет пишет, читаем в буфер
+	for  {
+		_, err := conn.Read(buf)
+		
+		// по возвращении клиентским сокетом EOF или другой ошибки логируем ошибку, так как не успели вычитать все данные, а клиент уже закрыл сокет
+		if err != nil {
+			log.Fatal(err)
+		}
+		
+		// по возвращении клиентским сокетом пустой строки, перестаем читать
+		if strings.Contains(string(buf), "\n\n") {
+			data += string(buf)
+			break
+		}
+		
+		// если размер данных больше, чем размер буфера
+		if len(buf) >= cap(buf) {
+			data += string(buf)
+			buf = make([]byte, len(buf))
+		}
+	}
+	return data
 }
